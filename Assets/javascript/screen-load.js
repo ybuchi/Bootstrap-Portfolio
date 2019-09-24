5e917e5ea4c191020cd1a0afe86ee35b44235e40
Yannik Buchi <ybuchi@ithaka.org>
2019-09-24 10:23:42 -0400

    Fixed the javascript functionality of the projects page to show most recent projects in the footer.

--- Assets/javascript/screen-load.js
+++ Assets/javascript/screen-load.js
@@ -15,6 +15,7 @@ var project4 = $("#card-title-4").text();
 //Create variables for the Project links
 
 var link1 = $("#project-link-1").attr("href");
+console.log(link1);
 var link2 = $("#project-link-2").attr("href");
 var link3 = $("#project-link-3").attr("href");
 var link4 = $("#project-link-4").attr("href");
@@ -22,18 +23,18 @@ var link4 = $("#project-link-4").attr("href");
 
 
 //Place these names inside of the links
-$("#project-name-1").append("<a>" + project1 + "</a>");
-$("#project-name-1").attr("href", link1);
+$("#link-1").append(project1);
+$("#link-1").attr("href", link1);
 
-$("#project-name-2").append(project2);
-$("#project-name-2").attr("href", link1);
-
-
-$("#project-name-3").append(project3);
-$("#project-name-3").attr("href", link1);
+$("#link-2").append(project2);
+$("#link-2").attr("href", link1);
 
 
-$("#project-name-4").append(project4);
-$("#project-name-4").attr("href", link1);
+$("#link-3").append(project3);
+$("#link-3").attr("href", link1);
+
+
+$("#link-4").append(project4);
+$("#link-4").attr("href", link1);
 
 
